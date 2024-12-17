cc2d66a4fcda9fe2c3d09fa2f9d35c0c069071c9
gitDominusoft <gitversioncontroll@gmail.com>
2024-12-17 15:50:06 +0100

    fix images

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,11 @@ window.onscroll = () => {
 // document.querySelector("#programet").scrollIntoView({
 //   behavior: "smooth",
 // });
+
+VanillaTilt.init(document.querySelector(".your-element"), {
+  max: 25,
+  speed: 400,
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".your-element"));
